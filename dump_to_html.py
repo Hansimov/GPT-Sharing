fea5eb5e90de2f950373b53794efbaffa2f90d23
Hansimov <591172499@qq.com>
2023-06-16 18:01:25 +0800

    [Refactor] Use OOP to refactor dump_to_html.py

--- dump_to_html.py
+++ dump_to_html.py
@@ -3,36 +3,50 @@ import cssutils
 import subprocess
 from pathlib import Path
 
-md_filename = "prompts-cheat-sheet.md"
-html_filename = Path(md_filename).stem + ".html"
-cmd_md_to_html = (
-    f"npx markmap-cli {md_filename} --no-toolbar --no-open -o {html_filename}"
-)
 
-css_filename = "./prompt-cheat-sheet-dark.css"
-subprocess.run(cmd_md_to_html, shell=True)
+class MD2HTML:
+    def __init__(self):
+        self.md_filename = "prompts-cheat-sheet.md"
+        self.html_filename = Path(self.md_filename).stem + ".html"
+        self.css_filename = "./prompt-cheat-sheet-dark.css"
 
-with open(html_filename, "r") as rf:
-    html_str = rf.read()
-soup = BeautifulSoup(html_str, "html.parser")
+    def convert_md_to_html(self):
+        cmd_md_to_html = (
+            f"npx markmap-cli {self.md_filename} -o {self.html_filename} "
+            f"--no-toolbar --no-open "
+        )
+        subprocess.run(cmd_md_to_html, shell=True)
 
-soup.title.string = "Prompt Engineering Cheat Sheet"
+    def parse_html(self):
+        with open(self.html_filename, "r") as rf:
+            html_str = rf.read()
+        self.soup = BeautifulSoup(html_str, "html.parser")
 
-css_link_tag = soup.new_tag("link", rel="stylesheet", href=css_filename)
-soup.head.append(css_link_tag)
+    def add_css_to_html(self):
+        css_link_tag = self.soup.new_tag("link", rel="stylesheet", href=css_filename)
+        self.soup.head.append(css_link_tag)
+        # css_str = cssutils.parseFile(css_filename)
+        # style_str = f"<style>\n{css_str.cssText.decode()}\n</style>"
+        # soup.head.append(BeautifulSoup(style_str, "html.parser"))
 
-favicon_link_tag = soup.new_tag(
-    "link",
-    rel="shortcut icon",
-    href="https://img.uxwing.com/wp-content/themes/uxwing/download/brands-social-media/chatgpt-icon.svg",
-)
-soup.head.append(favicon_link_tag)
+        self.soup.title.string = "Prompt Engineering Cheat Sheet"
 
+    def add_favicon_to_html(self):
+        favicon_link_tag = self.soup.new_tag(
+            "link",
+            rel="shortcut icon",
+            href="https://img.uxwing.com/wp-content/themes/uxwing/download/brands-social-media/chatgpt-icon.svg",
+        )
+        self.soup.head.append(favicon_link_tag)
 
-# css_str = cssutils.parseFile(css_filename)
-# style_str = f"<style>\n{css_str.cssText.decode()}\n</style>"
-# soup.head.append(BeautifulSoup(style_str, "html.parser"))
+    def dump_to_html(self):
+        new_html_str = str(self.soup)
+        with open(self.html_filename, "w") as wf:
+            wf.write(new_html_str)
 
-new_html_str = str(soup)
-with open(html_filename, "w") as wf:
-    wf.write(new_html_str)
+    def run(self):
+        self.convert_md_to_html()
+        self.parse_html()
+        self.add_css_to_html()
+        self.add_favicon_to_html()
+        self.dump_to_html()
